97c7f9510e7c0b6da0b6f36df855008e406b1e1a
James <Jpmsiska@gmail.com>
2023-02-16 14:35:21 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints the size of various types of variables
+ * Return: 0
+ */
+int main(void)
+{
+char j;
+int a;
+long int m;
+long long int e;
+float s;
+printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(j));
+Printf("size of a int: %lu byte(s)\n", (unsigned long) sizeof(a));
+Printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(m));
+Printf("size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(e));
+Printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(s));
+return (0);
+}
