b945cbdfc5894ee35994db4bb903d44f3d1cd397
James <Jpmsiska@gmail.com>
2023-02-16 15:19:39 -0800

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,19 @@
  */
 
 int main(void)
+
 {
+
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
 Printf("Size of a int: %zu byte(s)\n", sizeof(int));
+
 Printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+
 Printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+
 Printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
 return (0);
+
 }
