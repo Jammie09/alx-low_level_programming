7e8908ab2b58f97d2db1bdebc58f946ebb06af95
James <Jpmsiska@gmail.com>
2023-02-17 18:59:25 -0800

    qwerty

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,21 +14,19 @@ for (i = 0; i <= 98; i++)
 {
 for (j = i; j <= 99; j++)
 {
-if (i != j)
-{
 putchar('0' + i / 10);
 putchar('0' + i % 10);
 putchar(' ');
 putchar('0' + j / 10);
 putchar('0' + j % 10);
-if (i != 98 || j != 99)
-{
+if (i == 98 && j == 99)
+continue;
 putchar(',');
 putchar(' ');
+
 }
 }
-}
-}
+putchar('\n');
 return (0);
 }
 
