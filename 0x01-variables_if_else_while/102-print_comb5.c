8454c9fcbceab49792d377410260a3e14672ef9b
James <Jpmsiska@gmail.com>
2023-02-17 18:44:24 -0800

    qwerty

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,27 +5,27 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int i, j, k, l;
+int i, j;
 
 for (i = 0; i < 100; i++)
 {
-j = i / 10;
-k = i % 10;
-for (l = 0; l < 100; l++)
+for (j = i; j < 100; j++)
 {
-j = l / 10;
-k = l % 10;
+if (i != j)
+{
 putchar('0' + i / 10);
 putchar('0' + i % 10);
 putchar(' ');
-putchar('0' + l / 10);
-putchar('0' + l % 10);
-putchar(' ');
-if (l % 10 == 9)
+putchar('0' + j / 10);
+putchar('0' + j % 10);
+if (i != 98 || j != 99)
 {
-putchar('\n');
+putchar(',');
+putchar(' ');
+}
 }
 }
 }
