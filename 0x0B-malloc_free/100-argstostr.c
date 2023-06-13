43ea7c429d653625df68ebe9bc425ece29616743
Jammie09 <jpmsiska@gmail.com>
2023-06-13 01:44:51 -0700

    update

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,7 +5,7 @@
 * @ac: int input
 * @av: double pointer array
 * Return: 0
-*/
+**/
 char *argstostr(int ac, char **av)
 {
 int i, n, r = 0, l = 0;
@@ -16,23 +16,23 @@ for (i = 0; i < ac; i++)
 {
 for (n = 0; av[i][n]; n++)
 l++;
-}l
-+= ac;
+}
+l += ac;
 str = malloc(sizeof(char) * l + 1);
 if (str == NULL)
 return (NULL);
 for (i = 0; i < ac; i++)
-{ for (
-n
-=
-0; av[i][n]; n++)
+{
+for (n = 0; av[i][n]; n++)
 {
 str[r] = av[i][n];
 r++;
-} if (
+}
+if (
 str[r] == '\0')
 {
 str[r++] = '\n';
-}}
+}
+}
 return (str);
 }
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,9 +3,8 @@
 /**
 * count_word - helper function to count the number of words in a string
 * @s: string to evaluate
-**
-Return: number of words
-*/
+* Return: number of words
+**/
 int count_word(char *s)
 {
 int flag, c, w;
@@ -22,11 +21,11 @@ w++;
 }
 }
 return (w);
-} /**
+}
+/**
 * **strtow - splits a string into words
 * @str: string to split
-**
-Return: pointer to an array of strings (Success)
+* Return: pointer to an array of strings (Success)
 * or NULL (Error)
 */
 char **strtow(char *str)
